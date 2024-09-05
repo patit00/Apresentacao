19ff8e8145d9bd8e103c75dc84e14c8284c7ada1
patit00 <129318253+patit00@users.noreply.github.com>
2024-09-05 22:54:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const primeiroNome = "patricia;"
+const sobreNome = "suarez"
+const idade = 17;
+const cidadeNatal = "Barquisimeto";
+const altura = 1.67;
+
+
+console.log(primeiroNome);
+console.log("O meu primeiro nome é: ")
+
+
+console.log("O meu sobre nome é:")
+console.log(sobreNome);
+
+
+console.log("Minha ciade natal é:")
+console.log(idade);
+
+
+console.log("Minha cidade natal é:")
+console.log(cidadeNatal);
+
+
+console.log("Minha altura é")
+console.log(altura);
+
